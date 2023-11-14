a93ce8ab01313eaa94dda7dcd0d22bf0ff3848e6
letsjo <hyunoh.jo@gmail.com>
2023-11-14 15:05:12 +0900

    Feat: add mission constrints

---
+++ src/utils/constant.js
@@ -0,0 +1,11 @@
+export const MENU_LIST = [
+  { name: "해장국", cookTime: 2 },
+  { name: "순대국", cookTime: 1 },
+];
+
+export const CHEF_LIST = [{ name: "장금이" }, { name: "백주부" }];
+
+export const SERVER_LIST = [
+  { name: "서빙이", time: 1 },
+  { name: "카운터", time: 2 },
+];
