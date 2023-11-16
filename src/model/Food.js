364278db9018b97635e515082a965acd653b51e4
letsjo <hyunoh.jo@gmail.com>
2023-11-16 16:06:38 +0900

    Feat: add comment

--- src/model/Food.js
+++ src/model/Food.js
@@ -7,10 +7,16 @@ export class Food {
     this.#cookTime = cookTime;
   }
 
+  /**
+   * @returns {string} 음식 이름을 반환한다.
+   */
   getName() {
     return this.#name;
   }
 
+  /**
+   * @returns {number} 음식 조리 시간을 반환한다.
+   */
   getCookTime() {
     return this.#cookTime;
   }
