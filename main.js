4421ae81d7520ed5710d5e9c592ce725f4425b6d
letsjo <hyunoh.jo@gmail.com>
2023-11-16 13:10:03 +0900

    Refactor: 클릭시 시뮬레이션 시작 메인 로직 변경

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import { CookController } from './src/controller/CookController.js';
-import { OrderController } from './src/controller/OrderController.js';
-import { ServerController } from './src/controller/ServerController.js';
-import { Restaurant } from './src/model/Restaurant.js';
-import { CHEF_STATE, ORDER_STATE, SERVER_STATE } from './src/utils/constant.js';
-import { View } from './src/view/View.js';
+import { CookController } from "./src/controller/CookController.js";
+import { OrderController } from "./src/controller/OrderController.js";
+import { ServerController } from "./src/controller/ServerController.js";
+import { Restaurant } from "./src/model/Restaurant.js";
+import { CHEF_STATE, ORDER_STATE, SERVER_STATE } from "./src/utils/constant.js";
+import { View } from "./src/view/View.js";
 
 class Main {
   #restaurant;
@@ -17,55 +17,59 @@ class Main {
     this.CookController = new CookController(this.#restaurant, this.#view);
     this.ServerController = new ServerController(this.#restaurant, this.#view);
 
-    this.controllerArray = [this.OrderController, this.CookController, this.ServerController];
+    this.controllerArray = [
+      this.OrderController,
+      this.CookController,
+      this.ServerController,
+    ];
     this.initializeSimulation();
   }
 
   initializeSimulation() {
     this.#view.setClickOrder(this.handleOrderButtonClick.bind(this));
     this.controllerArray.forEach((controller) => controller.setPrepareOpen());
-    this.startSimulation();
   }
 
   handleOrderButtonClick(menuName) {
     const order = this.#restaurant.addOrder(menuName);
     this.#view.setAddOrder(order);
+    this.startSimulation();
   }
 
   startSimulation() {
-    setInterval(async () => {
-      const order = await this.OrderController.getNextOrder();
+    setTimeout(async () => {
       const chef = await this.CookController.findAvailable();
+      const order = await this.OrderController.getNextOrder();
       order.setState(ORDER_STATE.COOKING);
       this.#view.setUpdateOrderList(order);
       chef.setOrder(order);
       chef.setState(CHEF_STATE.COOKING);
       this.#view.setUpdateChef(chef);
-      chef.cook(order).then((order) => {
-        order.setState(ORDER_STATE.COOKED);
-        this.CookController.returnChef(chef);
-        this.#view.setUpdateChef(chef);
-        this.#view.setAddServer(order);
-        this.#view.deleteOrder(order);
-        this.#restaurant.addServe(order);
-      });
-    }, 100);
 
-    setInterval(async () => {
-      const order = await this.ServerController.getNextServe();
+      await chef.cook(order);
+
+      order.setState(ORDER_STATE.COOKED);
+      this.CookController.returnChef(chef);
+      this.#view.setUpdateChef(chef);
+      this.#view.setAddServer(order);
+      this.#view.deleteOrder(order);
+      this.#restaurant.addServe(order);
+
       const server = await this.ServerController.findAvailable();
+
       order.setState(ORDER_STATE.SERVING);
       server.setOrder(order);
       server.setState(SERVER_STATE.WAITING);
       this.#view.setUpdateServer(server);
-      server.serve(order).then((order) => {
-        order.setState(ORDER_STATE.DONE);
-        server.setState(SERVER_STATE.WAITING);
-        this.ServerController.returnServer(server);
-        this.#view.setUpdateServer(server);
-        server.setOrder(null);
-        this.#view.doneServer(order);
-      });
+
+      await server.serve(order);
+
+      order.setState(ORDER_STATE.DONE);
+      server.setState(SERVER_STATE.WAITING);
+      this.ServerController.returnServer(server);
+      this.#view.setUpdateServer(server);
+      server.setOrder(null);
+      this.#view.doneServer(order);
     }, 100);
   }
 }
