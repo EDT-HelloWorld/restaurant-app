ae10451df3fb4985456a5ce4c2c16628303c7165
letsjo <hyunoh.jo@gmail.com>
2023-11-16 16:10:07 +0900

    Feat: add comment

--- src/model/Chef.js
+++ src/model/Chef.js
@@ -13,35 +13,58 @@ export class Chef {
     this.#state = CHEF_STATE.WAITING;
   }
 
+  /**
+   * @returns {number} Chef에게 부여된 id를 반환한다.
+   */
   getId() {
     return this.#id;
   }
 
+  /**
+   * @returns {string} Chef의 이름을 반환한다.
+   */
   getName() {
     return this.#name;
   }
 
+  /**
+   * @returns {Order} Chef가 요리중인 Order를 반환한다.
+   */
   getOrder() {
     return this.#order;
   }
 
+  /**
+   * @returns {CHEF_STATE} Chef의 상태를 반환한다.
+   */
   getState() {
     return this.#state;
   }
 
+  /**
+   * @param {Order} order Chef가 요리할 Order를 설정한다.
+   */
   setOrder(order) {
     this.#order = order;
   }
 
+  /**
+   *
+   * @param {CHEF_STATE} state Chef의 상태를 설정한다.
+   */
   setState(state) {
     this.#state = state;
   }
 
-  cook(order) {
+  /**
+   * @param {Order} order 요리할 Order를 받아서 요리를 시작한다.
+   * @returns {Promise<Order>} 요리가 완료된 Order를 반환한다.
+   */
+  cook() {
     return new Promise((resolve) => {
       setTimeout(() => {
-        resolve(order);
-      }, order.getFood().getCookTime() * 1000);
+        resolve(this.getOrder());
+      }, this.getOrder().getFood().getCookTime() * 1000);
     });
   }
 }
